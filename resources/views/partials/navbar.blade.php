02399a17c62f4664be5552ed3c31c0f7e00b5b48
FrenchMajesty <verdi.kap@gmail.com>
2017-08-04 23:19:40 -0500

    Update navbar to support logged in users

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="./">
+    <a class="navbar-brand" href="{{ route('index') }}">
         <strong>Cool Website</strong>
     </a>
     <div class="collapse navbar-collapse">
@@ -13,17 +13,27 @@
             <li class="nav-item active">
                 <a href="./school" class="nav-link">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a href="./signup" class="nav-link">Sign Up</a>
-            </li>
-            <li class="nav-item dropdown btn-group">
-                <a class="nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
-                <div class="dropdown-menu dropdown-menu-right dropdown" aria-labelledby="dropdownMenu1">
-                    <a class="dropdown-item primary-hover">My Profile</a>
-                    <a class="dropdown-item primary-hover">Log out</a>
-                    <a class="dropdown-item primary-hover">Control Panel (Admin)</a>
-                </div>
-            </li>
+            @if (Auth::guest())
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="nav-link">Sign In</a>
+                </li>
+            @else
+                <li class="nav-item dropdown btn-group">
+                    <a class="nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown" aria-labelledby="dropdownMenu1">
+                        <a class="dropdown-item primary-hover">My Profile</a>
+                        <a href="{{ route('logout') }}" class="dropdown-item primary-hover" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                     Log out</a>
+                        @if(Auth::user()->account_type == 'admin') 
+                            <a class="dropdown-item primary-hover">Control Panel (Admin)</a>
+                        @endif
+                    </div>
+                </li>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            @endif
         </ul>
     </div>
 </nav>

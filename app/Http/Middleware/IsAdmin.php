6d410f543361a221f7e4994e0f54bd0b76338465
FrenchMajesty <verdi.kap@gmail.com>
2017-08-05 12:35:29 -0500

    Update middleware for users not logged in

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -15,7 +16,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user()->account_type != 'admin') {
+        if (!Auth::check() || $request->user()->account_type != 'admin') {
             return redirect('home');
         }
 

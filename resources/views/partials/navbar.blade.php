98ab420bac85203ccb35df5382f67a9a578cc298
FrenchMajesty <verdi.kap@gmail.com>
2017-08-14 19:27:48 -0500

    Use translations

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="{{ route('index') }}">
-        <strong>Cool Website</strong>
+        <strong>{{env('APP_NAME')}}</strong>
     </a>
     <div class="collapse navbar-collapse">
         <form class="form-inline mr-auto col-md-4 offset-md-3">
@@ -11,22 +11,21 @@
         </form>
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a href="./school" class="nav-link">Home <span class="sr-only">(current)</span></a>
+                <a href="./school" class="nav-link">{{__('home')}} <span class="sr-only">(current)</span></a>
             </li>
             @if (Auth::guest())
                 <li class="nav-item">
-                    <a href="{{ route('login') }}" class="nav-link">Sign In</a>
+                    <a href="{{ route('login') }}" class="nav-link">{{__('sign in')}}</a>
                 </li>
             @else
                 <li class="nav-item dropdown btn-group">
-                    <a class="nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Account</a>
+                    <a class="nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{__('account')}}</a>
                     <div class="dropdown-menu dropdown-menu-right dropdown" aria-labelledby="dropdownMenu1">
-                        <a class="dropdown-item primary-hover">My Profile</a>
-                        <a href="{{ route('logout') }}" class="dropdown-item primary-hover" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                     Log out</a>
+                        <a class="dropdown-item primary-hover">{{__('my profile')}}</a>
+                        <a href="{{ route('logout') }}" class="dropdown-item primary-hover" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                     {{__('log out')}}</a>
                         @if(Auth::user()->account_type == 'admin') 
-                            <a class="dropdown-item primary-hover">Control Panel (Admin)</a>
+                            <a class="dropdown-item primary-hover">{{__('control panel')}} ({{__('admin')}})</a>
                         @endif
                     </div>
                 </li>

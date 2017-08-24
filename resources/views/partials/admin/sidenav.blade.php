eb58d95e3b0855e93fc4d6726f4abc9662058a05
FrenchMajesty <verdi.kap@gmail.com>
2017-08-23 20:56:44 -0500

    Add routing to navigation

--- resources/views/partials/admin/sidenav.blade.php
+++ resources/views/partials/admin/sidenav.blade.php
@@ -1,25 +1,25 @@
 <div class="sidebar-wrapper">
     <ul class="nav">
-        <li class="active">
-            <a href="./admin">
+        <li {{ (\Request::route()->getName() == 'admin.index') ? 'class=active' : ''}}>
+            <a href="{{route('admin.index')}}">
                 <i class="material-icons">dashboard</i>
                 <p>{{__('dashboard')}}</p>
             </a>
         </li>
-        <li>
-            <a href="./users">
+        <li {{ (\Request::route()->getName() == 'admin.users') ? 'class=active' : ''}}>
+            <a href="{{route('admin.users')}}">
                 <i class="material-icons">person</i>
                 <p>{{__('Users')}}</p>
             </a>
         </li>
-        <li>
-            <a href="./prof">
+        <li {{ (\Request::route()->getName() == 'admin.profs') ? 'class=active' : ''}}>
+            <a href="{{route('admin.profs')}}">
                 <i class="material-icons">face</i>
                 <p>{{__('Profs')}}</p>
             </a>
         </li>
-        <li>
-            <a href="./school">
+        <li {{ (\Request::route()->getName() == 'admin.schools') ? 'class=active' : ''}}>
+            <a href="{{route('admin.schools')}}">
                 <i class="material-icons">account_balance</i>
                 <p>{{__('schools')}}</p>
             </a>

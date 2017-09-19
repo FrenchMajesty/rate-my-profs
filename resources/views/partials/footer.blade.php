c7b490cb6eb329c5797430b4702fc44d5ea65e29
FrenchMajesty <verdi.kap@gmail.com>
2017-09-18 19:50:45 -0500

    Hide footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,10 @@
-<footer class="page-footer blue center-on-small-only primary" style="padding: 0">
+<!--footer class="page-footer blue center-on-small-only primary" style="padding: 0">
 
-    <!--Copyright-->
     <div class="footer-copyright">
         <div class="container-fluid">
             &copy; <?php echo date('Y'); ?> Copyright. All Rights Reserved. Design by <a href="http://design-by-verdi.com" target="_blank">Verdi Co.</a>
 
         </div>
     </div>
-    <!--/.Copyright-->
 
-</footer>
+</footer-->
